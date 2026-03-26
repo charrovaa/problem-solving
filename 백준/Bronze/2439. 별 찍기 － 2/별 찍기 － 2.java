c48e489c5bdb3ac1fd4fa4094e28f23791a50141
charrovaa <charrovaa@gmail.com>
2026-03-26 17:40:16 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 112 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.StreamTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
             StreamTokenizer st = new StreamTokenizer(br);
             st.nextToken();
-            StringBuilder sb = new StringBuilder();
             for (int i = 0; i < st.nval; i++) {
                 for (int j = 1; j < st.nval - i; j++) {
-                    sb.append(" ");
+                    bw.append(" ");
                 }
                 for (int j = 0; j <= i; j++) {
-                    sb.append("*");
+                    bw.append("*");
                 }
-                sb.append("\n");
+                bw.append("\n");
             }
-            System.out.print(sb.toString());
         }
     }
 }
