a7f488dd0fd02b2d0c52eebfbb7d2af2a4379d63
charrovaa <charrovaa@gmail.com>
2026-03-26 14:34:57 +0900

    [Bronze III] Title: A+B - 6, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int count = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < count; i++) {
+                String[] inputs = reader.readLine().split(",");
+                int a = Integer.parseInt(inputs[0]);
+                int b = Integer.parseInt(inputs[1]);
+                System.out.println(a + b);
+            }
+        } catch (IOException e) {
+
+        }
+    }
+}
