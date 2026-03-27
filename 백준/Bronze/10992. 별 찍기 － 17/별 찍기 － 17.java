b0fae589458f140a1a94b60dc085d03c4d3cadfa
charrovaa <charrovaa@gmail.com>
2026-03-27 17:01:35 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 112 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int n = Integer.parseInt(br.readLine());
+            for (int i = 1; i < n; i++) {
+                for (int b = i; b < n; b++) {
+                    bw.append(" ");
+                }
+                if (i != 1) {
+                    bw.append("*");
+                }
+                for (int b = 0; b < (i - 1) * 2 - 1; b++) {
+                    bw.append(" ");
+                }
+                bw.append("*");
+                bw.append("\n");
+            }
+            for (int i = 0; i < n * 2 - 1; i++) {
+                bw.append("*");
+            }
+        }
+    }
+}
