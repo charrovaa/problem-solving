b4bdf0cd7117ee9df3be395c7f6b9c4fa5467586
charrovaa <charrovaa@gmail.com>
2026-03-29 16:56:42 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14404 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 1 || n == 2) {
+                bw.append(Integer.toString(n));
+                return;
+            }
+            int[] arr = new int[n + 1];
+            arr[1] = 1;
+            arr[2] = 2;
+            for (int i = 3; i <= n; i++) {
+                arr[i] = (arr[i - 2] + arr[i - 1]) % 10007;
+            }
+            bw.append(Integer.toString(arr[n]));
+        }
+    }
+}
