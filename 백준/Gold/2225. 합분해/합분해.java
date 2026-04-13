264a3b5313aedd87f6b3c47c6bea03515b050a9b
charrovaa <charrovaa@gmail.com>
2026-04-13 19:06:04 +0900

    [Gold V] Title: 합분해, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -12,31 +12,29 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken()); // 합
             int k = Integer.parseInt(st.nextToken()); // 정수의 개수
-            long[][] dp = new long[n + 2][k + 1];
+            int[][] dp = new int[n + 1][k + 1];
+            int MOD = 1_000_000_000;
 
-            /*
-            1. ㅁ을 1로 계산하여 정수의 합 표현
-            2. _를 ㅁ 사이에 배치해 각 정수의 크기를 결정
-            3. 제일 마지막 _ 고정
-            예시 : ㅁ_ _ㅁㅁ_ㅁ_ = 1 + 0 + 2 + 1
-            */
-
-            //dp[i][j] : i까지 j개를 고른 경우
-
-            for (int i = 0; i < n + 2; i++) {
-                dp[i][0] = 1;
+            // 정수의 개수가 1개인 (나 자신) 경우
+            for (int i = 1; i <= n; i++) {
+                dp[i][1] = 1;
             }
 
-            for (int i = 1; i <= n + 1; i++) { // 선택지 n + 1개 중
-                for (int j = 1; j <= k - 1; j++) { // k - 1개 선택
-                    for (int l = 0; l <= j; l++) {
-                        dp[i][j] += dp[i - 1][j - l];
-                        dp[i][j] %= 1_000_000_000;
-                    }
+            for (int i = 1; i <= k; i++) {
+                // 합이 1인데 정수의 개수가 2 이상인 경우
+                dp[1][i] = i;
+
+                // 합이 0인데 정수의 개수가 2 이상인 경우
+                dp[0][i] = 1;
+            }
+
+            // 새로운 숫자 추가 (마지막에 0을 두는 경우) + 이미 정해진 숫자의 크기 증가 (마지막 숫자에 1을 더하는 경우)
+            for (int i = 2; i <= n; i++) {
+                for (int j = 2; j <= k; j++) {
+                    dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % MOD;
                 }
             }
-
-            bw.write(String.valueOf(dp[n + 1][k - 1]));
+            bw.write(String.valueOf(dp[n][k]));
         }
     }
 }
