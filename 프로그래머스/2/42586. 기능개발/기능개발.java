6bbac50d7dc9928c8994ef322d9e30efa9acb8f7
charrovaa <charrovaa@gmail.com>
2026-04-25 16:16:49 +0900

    [level 2] Title: 기능개발, Time: 3.36 ms, Memory: 91.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,14 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] endDays = new int[101];
-        int day = 1;
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> answer = new ArrayList<>();
+
         for (int i = 0; i < progresses.length; i++) {
-            while (progresses[i] + speeds[i] * day < 100) {
-                day++;
-            }
-            endDays[day]++;
+            double remain = (100 - progresses[i]) / (double) speeds[i];
+            int day = (int) Math.ceil(remain);
+
+            if (!q.isEmpty() && q.peek() < day) { // 앞의 작업보다 더 오래 걸리면?
+                answer.add(q.size()); // 앞의 작업에 걸리는 시간을 answer에 저장 후
+                q.clear(); // 큐 비우기
+            } 
+            q.offer(day);
         }
-        return Arrays.stream(endDays).filter(i -> i != 0).toArray();
+        answer.add(q.size());
+        return answer.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
