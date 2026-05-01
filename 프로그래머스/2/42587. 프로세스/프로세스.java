45b37a01b4d80d2dc42d224ba8c390ecb07ecc56
charrovaa <charrovaa@gmail.com>
2026-05-01 17:01:41 +0900

    [level 2] Title: 프로세스, Time: 2.85 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,27 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        Queue<Integer> queue = new LinkedList<Integer>();
+        Queue<Integer> queue = new PriorityQueue<Integer>(Comparator.reverseOrder());
+        int answer = 0;
 
         for (int i : priorities) {
             queue.offer(i);
         }
 
-        Arrays.sort(priorities); // 오름차순 정렬
-
-        int answer = 0;
-
         while (!queue.isEmpty()) {
-            int element = queue.poll();
-            if (element == priorities[priorities.length - 1 - answer]) {
-                answer++;
-                if (--location < 0) {
-                    break;
-                }
-            } else {
-                queue.offer(element);
-                if (--location < 0) {
-                    location = queue.size() - 1;
+            for (int i = 0; i < priorities.length; i++) {
+                if (queue.peek() == priorities[i]) { // 배열의 값이 최댓값이라면?
+                    queue.poll();
+                    answer++;
+                    if (i == location) return answer;
                 }
             }
         }
