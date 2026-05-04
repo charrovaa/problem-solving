2359d502fd120c38b879630bf0c73820405fa95b
charrovaa <charrovaa@gmail.com>
2026-05-04 20:12:51 +0900

    [level 2] Title: 주식가격, Time: 17.38 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,14 +3,15 @@ class Solution {
 
         int[] answer = new int[prices.length];
 
-        for (int i = 0; i < answer.length - 1; i++) { // n - 1번째 원소까지 계산
+        for (int i = 0; i < answer.length; i++) {
             for (int j = i + 1; j < answer.length; j++) {
-                if (prices[i] > prices[j] || j == answer.length - 1) {
-                    answer[i] = j - i;
+                answer[i]++;
+                if (prices[i] > prices[j]) {
                     break;
                 }
             }
         }
+        
         return answer;
     }
 }
