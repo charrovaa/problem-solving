553478f04557e78bec677f9ebea246639bc2ba1b
charrovaa <charrovaa@gmail.com>
2026-05-06 12:52:16 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.41 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,15 +1,27 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
 
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-
-        for (int i = 0; i < completion.length; i++) {
-            if (!participant[i].equals(completion[i])) return participant[i];
+        for (String string : participant) {
+            if (map.containsKey(string)) {
+                int value = map.get(string);
+                map.replace(string, ++value);
+            } else {
+                map.put(string, 1);
+            }
         }
 
-        return participant[participant.length - 1];
+        for (String string : completion) {
+            int value = map.get(string);
+            if (value == 1) {
+                map.remove(string);
+            } else {
+                map.replace(string, --value);
+            }
+        }
+
+        return map.keySet().iterator().next();
     }
 }
