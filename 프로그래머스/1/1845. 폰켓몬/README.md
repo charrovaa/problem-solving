3f3a305aff65e47721082cdb7fee231ff443d78e
charrovaa <charrovaa@gmail.com>
2026-05-07 14:14:14 +0900

    [level 1] Title: 폰켓몬, Time: 4.61 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 76.5 MB, 시간: 3.14 ms
+메모리: 80.2 MB, 시간: 4.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 07일 11:39:49
+2026년 05월 07일 14:14:08
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,21 +1,13 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>(); // 몬스터 번호, 몬스터 수
-
-        for (int i : nums) {
-            if (map.containsKey(i)) {
-                int val = map.get(i);
-                map.put(i, ++val);
-            } else {
-                map.put(i, 1);
-            }
-        }
-
-        if (map.size() > nums.length / 2) return nums.length / 2;
-
-        return map.size();
+        return Arrays.stream(nums)
+                        .boxed()
+                        .collect(
+                            Collectors.collectingAndThen(
+                                Collectors.toSet(),
+                                set -> Integer.min(set.size(), nums.length / 2)));
     }
 }
