884d149c79fb9a786f44488c25ef1e3bf45b788f
charrovaa <charrovaa@gmail.com>
2026-05-11 11:57:27 +0900

    [level 2] Title: 전화번호 목록, Time: 143.15 ms, Memory: 220 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,12 +1,19 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public boolean solution(String[] phone_book) {
 
-        Arrays.sort(phone_book);
+        Map<String, Integer> map = new HashMap<String, Integer>();
 
-        for (int i = 1; i < phone_book.length; i++) {
-            if (phone_book[i].startsWith(phone_book[i - 1])) return false;
+        for (String string : phone_book) {
+            map.put(string, 1);
+        }
+
+        for (String string : phone_book) {
+            for (int i = 1; i < string.length(); i++) {
+                if (map.containsKey(string.substring(0, i))) return false;
+            }
         }
 
         return true;
