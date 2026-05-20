22115dcf980ac5c27b604b4b34f08e198d92165f
charrovaa <charrovaa@gmail.com>
2026-05-21 00:51:34 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 2.50 ms, Memory: 61.7 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#qna) 
 
 ### 성능 요약
 
-메모리: 62.2 MB, 시간: 5.24 ms
+메모리: 61.7 MB, 시간: 2.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 20일 20:12:15
+2026년 05월 21일 00:51:25
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,40 +2,49 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
+        // int[][] jobs = {}{요청 시점, 소요 시간}
+        int[][] ijobs = new int[jobs.length][3];
+        // int[][] ijobs = {}{소요 시간, 요청 시점, 인덱스}
+        for (int i = 0; i < ijobs.length; i++) {
+            ijobs[i][0] = jobs[i][1]; // 1순위 : 소요 시간
+            ijobs[i][1] = jobs[i][0]; // 2순위 : 요청 시점
+            ijobs[i][2] = i; // 3순위 : 인덱스
+        }
+        // 요청 시점에 따른 오름차순 정렬 (대기 큐에 들어가는 순서)
+        Arrays.sort(ijobs, (x, y) -> x[1] - y[1]);
 
-        // {요청 시점, 소요 시간}
-        Arrays.sort(jobs, (x, y) -> x[0] - y[0]); // 0. 요청 시점 기준 정렬
-        Queue<int[]> queue = new PriorityQueue<int[]>((x, y) -> x[1] - y[1]); // 1. 소요 시간 기준 정렬
-        int[] workingOn = null;
+        // 대기큐 (우선 순위에 따른 정렬)
+        Queue<int[]> pq = new PriorityQueue<int[]>((x, y) -> {
+            if (x[0] != y[0]) return x[0] - y[0];
+            else if (x[1] != y[1]) return x[1] - y[1];
+            else return x[2] - y[2];
+        });
 
-        int returnTime = 0;
+        // 이번에 대기열로 들어갈 작업의 인덱스
         int index = 0;
+        // 진행 시각
         int time = 0;
+        // 반환 시간의 합
+        int answer = 0;
 
-        while (workingOn != null || !queue.isEmpty() || index < jobs.length) {
-            while (index < jobs.length) {
-                if (jobs[index][0] == time) { // 같은 요청 시점을 갖고 있는 경우 해당 요청 시점일 때
-                    queue.add(jobs[index++]); // 대기 큐에 삽입
-                } else {
-                    break;
-                }
+        while (true) {
+            // 모든 작업들이 대기열에 들어갔으며 동시에 대기열이 비었을 때 종료
+            if (index == ijobs.length && pq.isEmpty()) break;
+
+            // 해당 시점에 대기열에 들어갈 수 있는 작업들 추가
+            while (index < ijobs.length && ijobs[index][1] <= time) {
+                pq.add(ijobs[index++]);
             }
 
-            if (workingOn == null && !queue.isEmpty()) {
-                workingOn = queue.poll();
-            }
-
-            time++;
-
-            if (workingOn != null) {
-                workingOn[1]--;
-                if (workingOn[1] == 0) {
-                    returnTime += time - workingOn[0];
-                    workingOn = null;
-                }
+            if (!pq.isEmpty()) {
+                int[] job = pq.poll();
+                time += job[0];
+                answer += time - job[1];
+            } else {
+                time = ijobs[index][1];
             }
         }
 
-        return returnTime / jobs.length;
+        return answer / ijobs.length;
     }
 }
