8c288db0e200dda466e882ea22d4cfe58d40d5d3
charrovaa <charrovaa@gmail.com>
2026-05-22 15:12:30 +0900

    [level 3] Title: 이중우선순위큐, Time: 23.72 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,29 +3,37 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
 
-        NavigableSet<Integer> nums = new TreeSet<Integer>();
+        NavigableMap<Integer, Integer> nums = new TreeMap<Integer, Integer>();
 
         for (String operation : operations) {
             if (operation.startsWith("I")) {
                 // 숫자 삽입
-                nums.add(Integer.parseInt(operation.substring(2)));
+                int num = Integer.parseInt(operation.substring(2));
+                nums.put(num, nums.getOrDefault(num, 0) + 1);
             } else {
                 if (nums.isEmpty()) continue;
+                int key;
                 if (operation.startsWith("D 1")) {
                     // 최댓값 삭제
-                    nums.remove(nums.last());
+                    key = nums.lastKey();
                 } else {
                     // 최솟값 삭제
-                    nums.remove(nums.first());
+                    key = nums.firstKey();
+                }
+                if (nums.get(key) == 1) {
+                    nums.remove(key);
+                } else {
+                    nums.put(key, nums.get(key) - 1);
                 }
             }
         }
 
         int[] answer = new int[2];
-        if (nums.isEmpty()) return answer;
-        
-        answer[0] = nums.last();
-        answer[1] = nums.first();
+        if (!nums.isEmpty()) {
+            answer[0] = nums.lastKey();
+            answer[1] = nums.firstKey();
+        }
+
         return answer;
     }
 }
