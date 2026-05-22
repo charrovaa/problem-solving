e653c0cd9d8ac13354ac24f907541605da3c797e
charrovaa <charrovaa@gmail.com>
2026-05-22 14:48:42 +0900

    [level 3] Title: 이중우선순위큐, Time: 17.97 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=java) 
 
 ### 성능 요약
 
-메모리: 97.5 MB, 시간: 45.02 ms
+메모리: 105 MB, 시간: 17.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 22일 01:16:03
+2026년 05월 22일 14:48:37
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,52 +3,29 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
 
-        Queue<Integer> maxpq = new PriorityQueue<Integer>(Collections.reverseOrder()); // 최대 힙
-        Queue<Integer> minpq = new PriorityQueue<Integer>(); // 최소 힙
-        Map<Integer, Integer> alive = new HashMap<Integer, Integer>(); // 지연 삭제 (값, 개수)
-        int count = 0;
+        NavigableSet<Integer> nums = new TreeSet<Integer>();
 
-        for (int i = 0; i < operations.length; i++) {
-            StringTokenizer st = new StringTokenizer(operations[i]);
-            if (st.nextToken().equals("I")) {
+        for (String operation : operations) {
+            if (operation.startsWith("I")) {
                 // 숫자 삽입
-                int number = Integer.parseInt(st.nextToken());
-                maxpq.add(number);
-                minpq.add(number);
-                alive.put(number, alive.getOrDefault(number, 0) + 1);
-                count++;
+                nums.add(Integer.parseInt(operation.substring(2)));
             } else {
-                int number;
-                if (count == 0) continue;
-                String token = st.nextToken();
-                while (true) {
-                    if (token.equals("1")) {
-                        number = maxpq.poll();
-                    } else {
-                        number = minpq.poll();
-                    }
-                    if (alive.get(number) != 0) break;
+                if (nums.isEmpty()) continue;
+                if (operation.startsWith("D 1")) {
+                    // 최댓값 삭제
+                    nums.remove(nums.last());
+                } else {
+                    // 최솟값 삭제
+                    nums.remove(nums.first());
                 }
-                alive.put(number, alive.get(number) - 1);
-                count--;
             }
         }
 
         int[] answer = new int[2];
-
-        if (count == 0) return answer;
-
-        answer[0] = Integer.MIN_VALUE;
-        answer[1] = Integer.MAX_VALUE;
-
-        for (int key : alive.keySet()) {
-            int value = alive.get(key);
-            if (value != 0) {
-                answer[0] = key > answer[0] ? key : answer[0];
-                answer[1] = key < answer[1] ? key : answer[1];
-            }
-        }
+        if (nums.isEmpty()) return answer;
         
+        answer[0] = nums.last();
+        answer[1] = nums.first();
         return answer;
     }
 }
