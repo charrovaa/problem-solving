567ad703cc03c144e3bd462c3c8c98fb4a704813
charrovaa <charrovaa@gmail.com>
2026-06-16 18:22:29 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.27 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전력망을 둘로 나누기 - 86971 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86971) 
 
 ### 성능 요약
 
-메모리: 79 MB, 시간: 1.93 ms
+메모리: 79.6 MB, 시간: 0.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 15일 18:34:11
+2026년 06월 16일 18:22:13
 
 ### 문제 설명
 
--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,50 +1,35 @@
-import java.util.*;
-
 class Solution {
-    List<Integer>[] graph;
+    int N;
+    int[][] adjacency;
     boolean[] visited;
-    int count;
+    int min;
     public int solution(int n, int[][] wires) {
-        graph = new ArrayList[n + 1]; // 인접 리스트
+        N = n;
+        adjacency = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
-        count = 0; // 한쪽 트리에 속한 노드 개수
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i < graph.length; i++) {
-            graph[i] = new ArrayList<Integer>();
-        }
-        for (int[] wire : wires) {
-            int a = wire[0];
-            int b = wire[1];
-
-            graph[a].add(b);
-            graph[b].add(a);
-        }
+        min = Integer.MAX_VALUE;
         for (int i = 0; i < wires.length; i++) {
-            int[] wire = wires[i];
-            int a = wire[0];
-            int b = wire[1];
-
-            // 연산
-            graph[a].remove(Integer.valueOf(b));
-            graph[b].remove(Integer.valueOf(a));
-            dfs(a);
-            min = Math.min(min, Math.abs(count - (n - count)));
-
-            // 초기화
-            count = 0;
-            Arrays.fill(visited, false);
-            graph[a].add(b);
-            graph[b].add(a);
-
+            int val_1 = wires[i][0];
+            int val_2 = wires[i][1];
+            adjacency[val_1][val_2] = adjacency[val_2][val_1] = 1;
         }
+
+        dfs(1);
+
         return min;
     }
 
-    void dfs(int towerNum) { // towerNum과 인접한 숫자의 개수를 카운트
+    int dfs(int towerNum) {
+        int count = 1;
         visited[towerNum] = true;
-        count++;
-        for (int next : graph[towerNum]) {
-            if (!visited[next]) dfs(next);
+        for (int i = 1; i <= N; i++) {
+            if (adjacency[towerNum][i] == 1 && !visited[i]) {
+                count+= dfs(i);
+            }
         }
+
+        min = Math.min(min, Math.abs(count - (N - count)));
+        
+        return count;
     }
 }
