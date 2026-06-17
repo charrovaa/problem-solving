0f87fdda5248d909115476dea26eb572de24fb0e
charrovaa <charrovaa@gmail.com>
2026-06-17 18:21:45 +0900

    [level 1] Title: 체육복, Time: 0.46 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,43 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        List<Integer> lostList = new ArrayList<>();
-        List<Integer> reserveList = new ArrayList<>();
+        Set<Integer> lostSet = new HashSet<>();
+        Set<Integer> reserveSet = new HashSet<>();
         int answer;
 
         for (int i : lost) {
-            lostList.add(i);
+            lostSet.add(i);
         }
 
         for (int i : reserve) {
-            reserveList.add(i);
+            reserveSet.add(i);
         }
-
-        // 정렬
-        Collections.sort(lostList);
-        Collections.sort(reserveList);
 
         // 중복 제거
-        for (int i = 1; i <= n; i++) {
-            if (lostList.contains(Integer.valueOf(i)) && reserveList.contains(Integer.valueOf(i))) {
-                lostList.remove(Integer.valueOf(i));
-                reserveList.remove(Integer.valueOf(i));
-            }
-        }
+        Set<Integer> overlap = new HashSet<>(lostSet);
+        overlap.retainAll(reserveSet); // 교집합
+        lostSet.removeAll(overlap); // 각각 교집합 지우기
+        reserveSet.removeAll(overlap);
 
-        answer = n - lostList.size();
+        answer = n - lostSet.size();
 
-        for (int i = 0; i < lostList.size(); i++) {
-            answer++;
-            if (reserveList.contains(lostList.get(i) - 1)) {
-                reserveList.remove(Integer.valueOf(lostList.get(i) - 1));
-            } else if (reserveList.contains(lostList.get(i))) {
-                reserveList.remove(Integer.valueOf(lostList.get(i)));
-            } else if (reserveList.contains(lostList.get(i) + 1)) {
-                reserveList.remove(Integer.valueOf(lostList.get(i) + 1));
-            } else {
-                answer--;
-            }
+        for (int i : lostSet) {
+            if (reserveSet.remove(i - 1) || reserveSet.remove(i + 1)) answer++;
         }
         
         return answer;
