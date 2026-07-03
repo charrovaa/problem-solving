da63681218773ca12bea064a919a459eb51194d3
charrovaa <charrovaa@gmail.com>
2026-07-03 16:06:52 +0900

    [level 2] Title: 구명보트, Time: 15.09 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int left = 0;
+        int right = people.length - 1;
+
+        Arrays.sort(people);
+
+        while (left <= right) {
+            if (left == right) {
+                answer++;
+                break;
+            }
+            if (people[left] + people[right] <= limit) left++;
+            right--;
+            answer++;
+        }
+
+        return answer;
+    }
+}
