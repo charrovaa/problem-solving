604aefee2707f42d0c8cb136c0a6cdbf02469631
charrovaa <charrovaa@gmail.com>
2026-07-08 16:22:59 +0900

    [level 3] Title: 단속카메라, Time: 4.79 ms, Memory: 58.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
         int answer = 1;
         int out = routes[0][1];
         for (int[] route : routes) {
