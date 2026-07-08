08e954a094e6c39db42687f814363f59decb60f4
charrovaa <charrovaa@gmail.com>
2026-07-08 16:21:58 +0900

    [level 3] Title: 단속카메라, Time: 4.87 ms, Memory: 58.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        int answer = 1;
+        int out = routes[0][1];
+        for (int[] route : routes) {
+            // 경로의 출발 지점이 카메라 범위가 아닌 경우
+            if (route[0] > out) {
+                out = route[1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
