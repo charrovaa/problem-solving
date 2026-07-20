1a54bc65770904333090b086882e2bddc4ab5fe6
charrovaa <charrovaa@gmail.com>
2026-07-20 12:29:05 +0900

    [level 3] Title: N으로 표현, Time: 17.54 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public static int solution(int N, int number) {
+        List<Set<Integer>> dp = new ArrayList<>();
+        dp.add(0, new HashSet<Integer>(Set.of(0)));
+
+        for (int i = 1; i <= 8; i++) {
+            Set<Integer> set = new HashSet<Integer>();
+            String n = new String();
+            for (int j = 1; j <= i; j++) {
+                n += N;
+            }
+            set.add(Integer.parseInt(n));
+            for (int j = 1; j < i; j++) {
+                for (Integer e1 : dp.get(j)) {
+                    for (Integer e2 : dp.get(i - j)) {
+                        set.add(e1 + e2);
+                        set.add(e1 - e2);
+                        set.add(e1 * e2);
+                        if (e2 != 0) {
+                            set.add(e1 / e2);
+                        }
+                    }
+                }
+            }
+            if (set.contains(number)) return i;
+            dp.add(i, set);
+        }
+
+        return -1;
+    }
+}
