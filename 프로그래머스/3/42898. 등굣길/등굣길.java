e8694a16db914c04ae46aac3b13a1205baf9d86b
charrovaa <charrovaa@gmail.com>
2026-07-22 14:54:51 +0900

    [level 3] Title: 등굣길, Time: 0.35 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,16 +1,16 @@
 class Solution {
+    private static final int MOD = 1_000_000_007;
     public int solution(int m, int n, int[][] puddles) {
-        final int MOD = 1_000_000_007;
         int[][] dp = new int[n + 1][m + 1];
-        boolean[][] isPuddle = new boolean[n + 1][m + 1];
 
         for (int[] puddle : puddles) {
-            isPuddle[puddle[1]][puddle[0]] = true;
+            dp[puddle[1]][puddle[0]] = -1;
         }
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                if (isPuddle[i][j]) {
+                if (dp[i][j] == -1) {
+                    dp[i][j] = 0;
                     continue;
                 }
                 if (i == 1 && j == 1) {
