1a016e73bb1be84ec1ff309b2c8f33380738a162
charrovaa <charrovaa@gmail.com>
2026-08-03 14:49:50 +0900

    [level 2] Title: 타겟 넘버, Time: 3.75 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,27 +1,25 @@
 class Solution {
     int[] numbers;
-    int target, count;
+    int target;
 
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
-        this.count = 0;
- 
-        dfs(0, numbers[0]);
-        dfs(0, -numbers[0]);
 
-        return count;
+        return dfs(0, 0);
     }
 
-    private void dfs(int index, int val) {
-
-        int cur = index + 1;
-
-        if (cur == numbers.length) {
-            if (val == target) count++;
+    // dfs(이번에 처리할 인덱스, 앞서 계산된 값)
+    private int dfs(int index, int val) {
+        int answer = 0;
+        if (index < numbers.length - 1) {
+            answer += dfs(index + 1, val + numbers[index]);
+            answer += dfs(index + 1, val - numbers[index]);
         } else {
-            dfs(cur, val + numbers[cur]);
-            dfs(cur, val - numbers[cur]);
+            // 마지막 인덱스인 경우 계산 후 리턴
+            if (val + numbers[index] == target) answer++;
+            if (val - numbers[index] == target) answer++;
         }
+        return answer;
     }
 }
