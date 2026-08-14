6514d9e31fcde560ff70d9af4cce1bcd64e85313
charrovaa <charrovaa@gmail.com>
2026-08-14 22:17:40 +0900

    [level 3] Title: 네트워크, Time: 0.12 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 0.15 ms
+메모리: 84.2 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 08월 14일 22:12:48
+2026년 08월 14일 22:17:36
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,16 +1,12 @@
 class Solution {
-    int[][] computers;
-    boolean[] visited;
-
     public int solution(int n, int[][] computers) {
-        this.computers = computers;
-        this.visited = new boolean[n];
+        boolean[] visited = new boolean[n];
         int network = 0;
 
         for (int i = 0; i < n; i++) {
             if (!visited[i]) { // 아직 방문하지 않은 컴퓨터인 경우
                 network++; // 새로운 네트워크로 간주
-                dfs(i); // 해당 컴퓨터에서 연결된 모든 컴퓨터를 재귀적으로 탐색
+                dfs(i, computers, visited); // 해당 컴퓨터에서 연결된 모든 컴퓨터를 재귀적으로 탐색
             }
         }
 
@@ -18,11 +14,11 @@ class Solution {
     }
 
     // 해당 컴퓨터와 직접 연결된 컴퓨터 탐색
-    private void dfs(int computer) {
+    private void dfs(int computer, int[][] computers, boolean[] visited) {
         visited[computer] = true; // 방문 완료
         for (int i = 0; i < visited.length; i++) {
             if (!visited[i] && computers[computer][i] == 1) { // 아직 방문 안 하고 연결되어 있는 경우
-                dfs(i); // 해당 컴퓨터 네트워크 탐색
+                dfs(i, computers, visited); // 해당 컴퓨터 네트워크 탐색
             }
         }
     }
