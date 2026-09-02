d27284205af9fae01cbe130941087d7536d7d0c8
charrovaa <charrovaa@gmail.com>
2026-09-02 23:18:07 +0900

    [level 3] Title: 여행경로, Time: 0.62 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+
+class Solution {
+
+    String[][] tickets;
+    String[] answer;
+    int index;
+    boolean flag;
+
+    public String[] solution(String[][] tickets) {
+        this.tickets = tickets;
+        this.answer = new String[tickets.length + 1];
+        this.index = 1;
+        this.flag = false;
+
+        Arrays.sort(tickets, (a, b) -> {
+            if (!a[0].equals(b[0])) {
+                return a[0].compareTo(b[0]);
+            }
+            return a[1].compareTo(b[1]);
+        });
+
+        answer[0] = "ICN";
+        dfs(answer[0]);
+        return answer;
+    }
+
+    private void dfs(String departure) {
+        if (index == answer.length) {
+            flag = true;
+            return;
+        }
+        
+        for (int i = 0; i < tickets.length; i++) {
+            if (tickets[i] != null && tickets[i][0].equals(departure)) {
+                String[] next = tickets[i];
+                tickets[i] = null; // 티켓 사용 완료
+                answer[index] = next[1];
+
+                dfs(answer[index++]);
+                if (flag) return;
+
+                tickets[i] = next; // 티켓 사용 철회
+                index--;
+            }
+        }
+    }
+}
