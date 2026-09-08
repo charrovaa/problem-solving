d4a6e04f63a50aeab15428383a320be244a27cfa
charrovaa <charrovaa@gmail.com>
2026-09-08 16:36:23 +0900

    [level 3] Title: 가장 먼 노드, Time: 32.44 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,34 +1,45 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int n, int[][] edge) {
-        
-        Queue<Integer> queue = new LinkedList<>();
-        int[] visited = new int[n + 1]; // 1 ~ n
-        Arrays.fill(visited, -1);
-        int max = 0;
+    public int solution(int n, int[][] edge) { // V, E
+        // 인접 리스트 생성
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) list.add(new ArrayList<Integer>()); // V
+        for (int[] e : edge) { // E
+            list.get(e[0]).add(e[1]);
+            list.get(e[1]).add(e[0]);
+        }
 
+        // 큐 생성
+        Queue<Integer> queue = new LinkedList<>();
+        int[] visited = new int[n + 1];
+        Arrays.fill(visited, -1); // V
+
+        // 초기화
         queue.offer(1);
         visited[1] = 0;
 
-        while (!queue.isEmpty()) {
+        // 반환값
+        int cnt = 0;
+        int max = 0;
+
+        // BFS
+        while (!queue.isEmpty()) { // V
             int cur = queue.poll();
-            for (int[] e : edge) {
-                if (e[0] != cur && e[1] != cur) continue;
-                int next = (e[0] != cur) ? e[0] : e[1];
-                if (visited[next] != -1) continue;
-                queue.offer(next);
-                visited[next] = visited[cur] + 1;
-                max = visited[next];
+            for (int next : list.get(cur)) { // 2E (E)
+                if (visited[next] == -1) {
+                    visited[next] = visited[cur] + 1;
+                    max = visited[next];
+                    queue.offer(next);
+                }
             }
         }
 
-        int answer = 0;
-
-        for (int i : visited) {
-            if (i == max) answer++;
+        // 해당 노드 카운트
+        for (int i : visited) { // V
+            if (i == max) cnt++;
         }
 
-        return answer;
+        return cnt;
     }
 }
