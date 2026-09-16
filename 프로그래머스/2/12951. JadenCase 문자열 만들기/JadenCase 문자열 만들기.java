8da5f40cfdc5107d8af790ad464bf62488b9d2e2
charrovaa <charrovaa@gmail.com>
2026-09-16 11:53:00 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.89 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,24 +1,13 @@
 class Solution {
     public String solution(String s) {
 
-        String[] ss = s.split("");
         String answer = "";
+        String[] ss = s.toLowerCase().split("");
         boolean isFirst = true;
 
         for (String string : ss) {
-
-            char c = string.charAt(0);
-
-            if (c == ' ') {
-                isFirst = true;
-            } else if (isFirst) {
-                c = Character.toUpperCase(c);
-                isFirst = false;
-            } else {
-                c = Character.toLowerCase(c);
-            }
-
-            answer += String.valueOf(c);
+            answer += isFirst ? string.toUpperCase() : string;
+            isFirst = string.equals(" ");
         }
 
         return answer;
