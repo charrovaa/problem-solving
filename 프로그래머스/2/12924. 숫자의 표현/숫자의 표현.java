f42c6587cba660391255bf63daa4526be572d98a
charrovaa <charrovaa@gmail.com>
2026-09-17 10:58:28 +0900

    [level 2] Title: 숫자의 표현, Time: 0.10 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,18 +1,8 @@
 class Solution {
     public int solution(int n) {
+        // 주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수 = 주어진 자연수의 홀수 약수의 개수
         int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            int num = 0;
-            for (int j = i; j <= n; j++) {
-                if (num + j < n) num += j;
-                else if (num + j == n) {
-                    answer++;
-                    break;
-                } else {
-                    break;
-                }
-            }
-        }
+        for (int i = 1; i <= n; i += 2) if (n % i == 0) answer++;
         return answer;
     }
 }
