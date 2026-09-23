3cf0310c7b59963c48b5172371aab646193333ca
charrovaa <charrovaa@gmail.com>
2026-09-23 11:10:10 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -5,9 +5,8 @@ class Solution {
         Set<String> set = new HashSet<>();
         set.add(words[0]);
         for (int i = 1; i < words.length; i++) {
-            if (set.contains(words[i]) ||
+            if (!set.add(words[i]) ||
                 words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) return new int[]{i % n + 1, i / n + 1};
-            set.add(words[i]);
         }
         return new int[]{0, 0};
     }
