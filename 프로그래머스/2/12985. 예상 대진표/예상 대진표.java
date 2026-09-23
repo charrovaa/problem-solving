7bda536955b4838e77337c5a4f4efce7488be8be
charrovaa <charrovaa@gmail.com>
2026-09-24 02:04:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,25 +2,12 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int cnt = 1;
-
-        for (int i = n; i > 0; i /= 2) {
-            if (opponent(a) == b) break;
-            a = nextNum(a);
-            b = nextNum(b);
-            cnt++;
+        int round = 0;
+        while (a != b) {
+            a = a / 2 + a % 2;
+            b = b / 2 + b % 2;
+            round++;
         }
-
-        return cnt;
-    }
-
-    private int opponent(int num) {
-        if (num % 2 == 0) return num - 1;
-        return num + 1;
-    }
-
-    private int nextNum(int num) {
-        if (num % 2 == 0) return num / 2;
-        return num / 2 + 1;
+        return round;
     }
 }
